25f6253d67e4480fd56886b4877e4da9c39900b4
John Prutton <johnrprutton@gmail.com>
2023-08-24 10:51:13 +0200

    Fix: Responsive nav and hero section (#2)

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,36 +1,50 @@
 import Image from "next/image"
 import Link from "next/link"
-import heroImg from "@/../public/dps-ceilings-1.jpg"
+
 import { Button } from "@/components/ui/button"
-import { InfoSection } from "./info-section"
+import heroImg from "@/../public/dps-ceilings-1.jpg"
 
 export function Hero() {
 	return (
-		<InfoSection img={heroImg}>
-			<h1 className="font-black text-5xl text-primary mb-8">
-				STRETCH CEILINGS & WALLS
-			</h1>
+		<article className="w-full h-screen relative">
+			{/* Background image */}
+			<Image
+				src={heroImg.src}
+				alt="DPS stretch ceiling"
+				placeholder="blur"
+				blurDataURL={heroImg.blurDataURL}
+				fill
+				priority={true}
+				className="object-cover brightness-30"
+			/>
 
-			<p className="leading-loose tracking-widest">
-				DPS is a world leader in stretch ceilings and wall
-				manufacturing. For 25 years we have helped bring to life amazing
-				design ideas and have created some of the most stunning spaces.
-				Over 1 000 000 m2 of DPS stretch ceilings have been installed
-				across the world in both commercial and residential areas. DPS
-				stretch ceilings and walls are 100% recyclable and covered by
-				industry standard certificates (CE, Fire classification etc).
-				All DPS stretch ceilings and walls have a 10-year warranty.
-			</p>
+			{/* Floating Content */}
+			<div className="absolute w-90% left-1/2 -translate-x-1/2 bottom-1/10 sm:bottom-1/3 sm:translate-y-1/2 sm:w-2xl sm:max-w-90vw">
+				<h1 className="font-black text-2xl text-primary sm:text-5xl">
+					STRETCH CEILINGS & WALLS
+				</h1>
 
-			<div className="flex flex-row w-full children:w-full gap-4 mt-4">
-				<Button size={"lg"} asChild>
-					<Link href={"/contact"}>Contact Us</Link>
-				</Button>
+				<p className="tracking-wide my-4 sm:my-8 sm:leading-loose">
+					For 25 years we have helped bring to life amazing design
+					ideas.
+					<span className="lt-sm:hidden">
+						{" "}
+						Over 1 000 000 m2 of DPS stretch ceilings have been
+						installed across the world in both commercial and
+						residential areas.
+					</span>
+				</p>
 
-				<Button size={"lg"} variant={"secondary"} asChild>
-					<Link href={"/gallery"}>View Gallery</Link>
-				</Button>
+				<div className="flex flex-col sm:flex-row w-full children:w-full gap-4">
+					<Button asChild>
+						<Link href={"/contact"}>Contact Us</Link>
+					</Button>
+
+					<Button variant={"secondary"} asChild>
+						<Link href={"/gallery"}>View Gallery</Link>
+					</Button>
+				</div>
 			</div>
-		</InfoSection>
+		</article>
 	)
 }
--- src/components/main-navigation.tsx
+++ src/components/main-navigation.tsx
@@ -1,49 +1,65 @@
 import Image from "next/image"
+import Link from "next/link"
+
 import {
 	NavigationMenu,
 	NavigationMenuList,
-	NavigationMenuItem,
-	NavigationMenuTrigger,
-	NavigationMenuContent,
-	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
 
 import { links } from "@/lib/sitemap"
-
 import logo from "@/../public/logo.png"
-import Link from "next/link"
-import { Button } from "./ui/button"
+
+import { MenuIcon } from "lucide-react"
 
 export default function MainNav() {
 	return (
-		<div className="backdrop-blur bg-background/50 fixed top-0 w-full z-1">
-			<div className="container mx-auto p-8 flex flex-row items-center">
-				<Link href={"/"}>
-					<Image
-						src={logo.src}
-						alt="DPS Logo"
-						width={logo.width}
-						height={logo.height}
-					/>
+		<div className="backdrop-blur bg-background/30 fixed top-0 w-full z-1">
+			<div className="container mx-auto px-2 py-4 sm:px-4 sm:py-8 flex flex-row items-center justify-between">
+				<Link href={"/"} className="relative aspect-110/58 h-8 sm:h-12">
+					<Image src={logo.src} alt="DPS Logo" fill />
 				</Link>
-				<NavigationMenu className="max-w-full children:w-full">
+
+				<NavigationMenu className="lt-sm:hidden max-w-full children:w-full">
 					<NavigationMenuList className="justify-evenly">
-						{links.map(({ title, link }, i) => (
-							<Button asChild variant={"ghost"} key={i}>
-								<Link
-									href={link}
-									className="text-4.25!"
-									{...(title == "Sample Book" && {
-										target: "_blank",
-									})}
-								>
-									{title.toUpperCase()}
-								</Link>
-							</Button>
-						))}
+						<Links />
 					</NavigationMenuList>
 				</NavigationMenu>
+
+				<Sheet>
+					<SheetTrigger className="sm:hidden">
+						<MenuIcon />
+					</SheetTrigger>
+					<SheetContent className="flex flex-col flex-1 justify-center gap-8 items-center children:text-center">
+						<Links />
+					</SheetContent>
+				</Sheet>
 			</div>
 		</div>
+	)
+}
+
+function Links() {
+	return (
+		<>
+			{links.map(({ title, link }, i) => (
+				<Button
+					asChild
+					variant={title === "Contact Us" ? "default" : "ghost"}
+					key={i}
+				>
+					<Link
+						href={link}
+						className="sm:text-4.25!"
+						{...(title == "Sample Book" && {
+							target: "_blank",
+						})}
+					>
+						{title}
+					</Link>
+				</Button>
+			))}
+		</>
 	)
 }
